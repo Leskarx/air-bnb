8e65800ec182049f469ddbbec0ae03c060cb82fe
Leskarx <leskarzone123@gmail.com>
2024-02-21 22:25:53 +0530

    slight input changes

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -10,7 +10,7 @@ const [input,setInput]=useState("")
         setInput(event.target.value)
 
       }} placeholder=" " id={placeholder} className=' peer w-full px-2 py-3 rounded-lg border border-black outline-none' />
-      <label for={placeholder}  className={(input=="")?" cursor-text transform duration-150 transtion z-10 text-md  peer-focus:scale-75 peer-focus:top-0  absolute top-1/2 left-2 -translate-y-1/2  origin-[0] bg-white":"z-10 text-md scale-75 absolute top-0 left-2 -translate-y-1/2   origin-[0] bg-white"}>{placeholder}</label>
+      <label for={placeholder}  className={(input=="")?" cursor-text transform duration-150 transtion z-10 text-md peer-focus:bg-white  peer-focus:scale-75 peer-focus:top-0  absolute top-1/2 left-2 -translate-y-1/2  origin-[0] ":"z-10 text-md scale-75 absolute top-0 left-2 -translate-y-1/2   origin-[0] bg-white"}>{placeholder}</label>
       
     </div>
   )
