e9287b7d25ab5bfb9fd7c62e78a7945de3ee6e25
Leskarx <leskarzone123@gmail.com>
2024-02-21 21:21:37 +0530

    added animation to PopOutScreen

--- src/components/popOutScreen/PopOutScreen.jsx
+++ src/components/popOutScreen/PopOutScreen.jsx
@@ -3,14 +3,42 @@ import React from 'react'
 import {IoMdClose} from "react-icons/io" 
 import ButtonC from './ButtonC'
 import {usePopOut} from "../../context/popOutContext"
-
+import { motion } from 'framer-motion';
 export default function PopOutScreen({title="Hello world",buttonPlaceHolder,body}) {
   const {isOpen,toggleIsOpen}=usePopOut()
+  const variants = {
+    open: { 
+      opacity: 1,
+       y: 0
+       },
+    closed: {
+       opacity: 0,
+        y:100 
+       },
+  }
   return (
     <div className=" fixed bg-neutral-800/70 z-50 inset-0">
      <div className='flex items-center justify-center w-full h-full'>
-      <section className='  w-full sm:w-[45%] h-screen sm:h-auto sm:relative border-b-[1px]  rounded-md shadow-md bg-white'>
-      <header className='flex p-6 items-center justify-center'>
+      <motion.div className='  w-full sm:w-[45%] h-screen sm:h-auto sm:relative border-b-[1px]  rounded-md shadow-md bg-white'
+      initial={{
+        y:100,
+        opacity:0,
+        // scale:0.8
+      }}
+      exit={{ opacity: 0,
+        y:-100
+      
+      }}
+      animate={isOpen ? "open" : "closed"}
+      variants={variants}
+      transition={{
+        delay:0.0300
+      }}
+    
+      
+
+      >
+      <header className='flex sm:mt-0 border-b-2  mt-5 p-6 items-center justify-center'>
         <button onClick={()=>{
           toggleIsOpen()
         }} className=' absolute p-1 border-0 hover:opacity-70 
@@ -33,7 +61,7 @@ export default function PopOutScreen({title="Hello world",buttonPlaceHolder,body
 
       </footer>
 
-      </section>
+      </motion.div>
     
 
      </div>
